0296e278c1075254e40f0365066bf43280a1058b
bradlinard <brad.linard@idelata.com>
2017-12-21 14:37:47 -0800

    plugin build working again

--- src/index.android.ts
+++
@@ -1,7 +0,0 @@
-import { AppCenter } from "./appcenter/android/appcenter.android";
-import { Analytics } from "./appcenter-analytics/android/analytics.android";
-import { Crashes } from "./appcenter-crashes/android/crashes.android";
-
-export namespace AppCenter {}
-export namespace Analytics {}
-export namespace Crashes {}
--- src/index.ios.ts
+++
@@ -1,7 +0,0 @@
-import { AppCenter } from "./appcenter/ios/appcenter.ios";
-import { Analytics } from "./appcenter-analytics/ios/analytics.ios";
-import { Crashes } from "./appcenter-crashes/ios/crashes.ios";
-
-export namespace AppCenter {}
-export namespace Analytics {}
-export namespace Crashes {}
